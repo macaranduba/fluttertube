2cdb4e7a83f1d4da4c9af0675d848c679272e2f9
José LR Reis <jreis78pt@gmail.com>
2021-04-10 15:05:09 -0300

    Criando e Configurando o Projeto

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertube/screens/home.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'FlutterTube',
-      home: Container(),
+      home: Home(),
     );
   }
 }
